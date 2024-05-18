bdda44de39848385e8851f8f9625ae04579ddef1
unknown <deepak.mahana@franchisediscovery.in>
2024-05-18 18:46:03 +0530

    bootstrap spring boot txnroutine service

---
+++ README.md
@@ -0,0 +1,9 @@
+# Transaction Service
+
+## Requirements
+- Java 11+
+- Maven
+
+## Setup
+1. Clone the repository
+   ```sh git clone https://github.com/DeepakMahana/txnroutine.git
---
+++ src/test/java/com/pismo/txnroutine/TxnroutineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pismo.txnroutine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TxnroutineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
