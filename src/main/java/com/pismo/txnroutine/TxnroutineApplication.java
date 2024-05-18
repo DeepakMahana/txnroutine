9d4c15ddb94a81d4691e85cb68a61f0c77c8ad14
unknown <deepak.mahana@franchisediscovery.in>
2024-05-18 18:46:03 +0530

    bootstrap spring boot txnroutine service

---
+++ src/main/java/com/pismo/txnroutine/TxnroutineApplication.java
@@ -0,0 +1,13 @@
+package com.pismo.txnroutine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxnroutineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TxnroutineApplication.class, args);
+	}
+
+}
